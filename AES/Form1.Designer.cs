01fa71736c07a379d41113e93724adfade2a0429
chiendz <chienlovedta@gmail.com>
2024-11-16 08:41:59 +0700

    1

---
+++ AES/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace AES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.INPUT = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_ma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_khoa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rad256 = new System.Windows.Forms.RadioButton();
+            this.rad192 = new System.Windows.Forms.RadioButton();
+            this.rad128 = new System.Windows.Forms.RadioButton();
+            this.txt_ro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_giai_thich = new System.Windows.Forms.TextBox();
+            this.INPUT.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // INPUT
+            // 
+            this.INPUT.Controls.Add(this.button4);
+            this.INPUT.Controls.Add(this.button3);
+            this.INPUT.Controls.Add(this.button2);
+            this.INPUT.Controls.Add(this.button1);
+            this.INPUT.Controls.Add(this.txt_ma);
+            this.INPUT.Controls.Add(this.label3);
+            this.INPUT.Controls.Add(this.txt_khoa);
+            this.INPUT.Controls.Add(this.label2);
+            this.INPUT.Controls.Add(this.rad256);
+            this.INPUT.Controls.Add(this.rad192);
+            this.INPUT.Controls.Add(this.rad128);
+            this.INPUT.Controls.Add(this.txt_ro);
+            this.INPUT.Controls.Add(this.label1);
+            this.INPUT.Dock = System.Windows.Forms.DockStyle.Left;
+            this.INPUT.Location = new System.Drawing.Point(0, 0);
+            this.INPUT.Margin = new System.Windows.Forms.Padding(2);
+            this.INPUT.Name = "INPUT";
+            this.INPUT.Padding = new System.Windows.Forms.Padding(2);
+            this.INPUT.Size = new System.Drawing.Size(298, 640);
+            this.INPUT.TabIndex = 0;
+            this.INPUT.TabStop = false;
+            this.INPUT.Text = "INPUT";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(139, 337);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 19);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 337);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 19);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 301);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 19);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Giải mã";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 301);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 19);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Mã hóa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txt_ma
+            // 
+            this.txt_ma.Location = new System.Drawing.Point(39, 258);
+            this.txt_ma.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ma.Name = "txt_ma";
+            this.txt_ma.Size = new System.Drawing.Size(229, 20);
+            this.txt_ma.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 223);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bản mã";
+            // 
+            // txt_khoa
+            // 
+            this.txt_khoa.Location = new System.Drawing.Point(41, 188);
+            this.txt_khoa.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_khoa.Name = "txt_khoa";
+            this.txt_khoa.Size = new System.Drawing.Size(227, 20);
+            this.txt_khoa.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Khóa";
+            // 
+            // rad256
+            // 
+            this.rad256.AutoSize = true;
+            this.rad256.Location = new System.Drawing.Point(178, 113);
+            this.rad256.Margin = new System.Windows.Forms.Padding(2);
+            this.rad256.Name = "rad256";
+            this.rad256.Size = new System.Drawing.Size(57, 17);
+            this.rad256.TabIndex = 3;
+            this.rad256.TabStop = true;
+            this.rad256.Text = "256-bit";
+            this.rad256.UseVisualStyleBackColor = true;
+            // 
+            // rad192
+            // 
+            this.rad192.AutoSize = true;
+            this.rad192.Location = new System.Drawing.Point(111, 113);
+            this.rad192.Margin = new System.Windows.Forms.Padding(2);
+            this.rad192.Name = "rad192";
+            this.rad192.Size = new System.Drawing.Size(57, 17);
+            this.rad192.TabIndex = 2;
+            this.rad192.TabStop = true;
+            this.rad192.Text = "192-bit";
+            this.rad192.UseVisualStyleBackColor = true;
+            // 
+            // rad128
+            // 
+            this.rad128.AutoSize = true;
+            this.rad128.Location = new System.Drawing.Point(39, 113);
+            this.rad128.Margin = new System.Windows.Forms.Padding(2);
+            this.rad128.Name = "rad128";
+            this.rad128.Size = new System.Drawing.Size(57, 17);
+            this.rad128.TabIndex = 2;
+            this.rad128.TabStop = true;
+            this.rad128.Text = "128-bit";
+            this.rad128.UseVisualStyleBackColor = true;
+            // 
+            // txt_ro
+            // 
+            this.txt_ro.Location = new System.Drawing.Point(39, 52);
+            this.txt_ro.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ro.Name = "txt_ro";
+            this.txt_ro.Size = new System.Drawing.Size(229, 20);
+            this.txt_ro.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bản rõ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_giai_thich);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(302, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(794, 640);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "OUTPUT";
+            // 
+            // txt_giai_thich
+            // 
+            this.txt_giai_thich.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txt_giai_thich.Location = new System.Drawing.Point(2, 17);
+            this.txt_giai_thich.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_giai_thich.Multiline = true;
+            this.txt_giai_thich.Name = "txt_giai_thich";
+            this.txt_giai_thich.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_giai_thich.Size = new System.Drawing.Size(790, 621);
+            this.txt_giai_thich.TabIndex = 0;
+            this.txt_giai_thich.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 640);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.INPUT);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.INPUT.ResumeLayout(false);
+            this.INPUT.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox INPUT;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_ma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_khoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rad256;
+        private System.Windows.Forms.RadioButton rad192;
+        private System.Windows.Forms.RadioButton rad128;
+        private System.Windows.Forms.TextBox txt_ro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_giai_thich;
+    }
+}
+
---
+++ AES/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES
+{
+    public partial class Form1 : Form
+    {
+       byte[,] SBox = new byte[16, 16] {
+    { 0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76 },
+    { 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0 },
+    { 0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15 },
+    { 0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75 },
+    { 0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84 },
+    { 0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF },
+    { 0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8 },
+    { 0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2 },
+    { 0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73 },
+    { 0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB },
+    { 0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79 },
+    { 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08 },
+    { 0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A },
+    { 0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E },
+    { 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF },
+    { 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16 }
+};
+
+
+
+        public int [,]mixColumnMatrix = {
+    { 2, 3, 1, 1 },
+    { 1, 2, 3, 1 },
+    { 1, 1, 2, 3 },
+    { 3, 1, 1, 2 }
+};
+    public static byte[] Rcon = new byte[]
+{
+    0,0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36
+};
+
+        public static Dictionary<int, string> hexDict = new Dictionary<int, string>()
+        {
+            { 0, "0" },
+            { 1, "1" },
+            { 2, "2" },
+            { 3, "3" },
+            { 4, "4" },
+            { 5, "5" },
+            { 6, "6" },
+            { 7, "7" },
+            { 8, "8" },
+            { 9, "9" },
+            { 10, "A" },
+            { 11, "B" },
+            { 12, "C" },
+            { 13, "D" },
+            { 14, "E" },
+            { 15, "F" }
+        };
+        public int numE;
+    public string []k = new string[15];
+    public string []w = new string[100];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string CharToHex(Byte c)
+        { 
+            string temp = "";
+            if (c < 10)
+                return "0" + hexDict[c];
+            return hexDict[c / 16] + hexDict[c % 16];
+        }
+        public string RotWord(string s)
+        {
+            if (s.Length != 4)
+                return "";
+            return s.Substring(1) + s[0];
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public string xor(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return ""; // Trả về chuỗi rỗng nếu độ dài không khớp
+
+            StringBuilder s3 = new StringBuilder();  // Dùng StringBuilder để xây dựng chuỗi kết quả
+            for (int i = 0; i < s1.Length; i++)
+            {
+                // Chuyển ký tự thành byte và thực hiện phép XOR
+                byte byte1 = (byte)s1[i];
+                byte byte2 = (byte)s2[i];
+                byte result = (byte)(byte1 ^ byte2);
+
+                // Chuyển byte kết quả thành ký tự và thêm vào chuỗi kết quả
+                s3.Append((char)result);
+            }
+            return s3.ToString();
+        }
+
+
+
+        public string SubByte(string s)
+        {
+            string c = "";
+            for (int i = 0; i < s.Length; ++i)
+            {
+                // Chuyển ký tự a[i] sang giá trị số nguyên
+                byte value = (byte)s[i];
+
+                // Tách hàng và cột từ giá trị (giả sử input trong dải 0-255)
+                int row = value / 16;        // Lấy 4 bit cao
+                int col = value % 16;     // Lấy 4 bit thấp
+
+                // Tra cứu giá trị trong SBox
+                byte sboxValue = SBox[row, col];
+
+                // Thêm giá trị S-box vào kết quả dưới dạng hex
+                char t = (char)sboxValue;
+                c += t;
+            }
+            return c;
+        }
+
+        public string XorRcon(string s, int i)
+        {
+            // Kiểm tra tham số đầu vào
+            if (string.IsNullOrEmpty(s) || s.Length < 4)
+                throw new ArgumentException("Chuỗi đầu vào phải có ít nhất 4 ký tự.");
+
+            if (i <= 0 || i > Rcon.Length)
+                throw new ArgumentOutOfRangeException("Chỉ số i không hợp lệ.");
+
+            // Lấy ký tự đầu tiên của chuỗi và XOR với giá trị rcon
+            char c = (char)((byte)s[0] ^ Rcon[i]);
+
+            // Ghép ký tự mới với các ký tự còn lại
+            return c.ToString() + s.Substring(1, 3);
+        }
+
+        public string g(string s, int i) 
+        {
+            string temp = RotWord(s);
+            //txt_giai_thich.Text += $"saurotword g[{i * 4 - 1}] = {StringToHex(temp)}\r\n";
+            temp = SubByte(temp);
+           // txt_giai_thich.Text += $"sausubyte g[{i * 4 - 1}] = {StringToHex(temp)}  {temp}\r\n";
+            temp = XorRcon(temp,i);
+           // txt_giai_thich.Text += $"g[{i*4 - 1}] = {StringToHex(temp)}\r\n";
+            return temp;
+        }
+
+        public int[] shift_row(int[] a, int b) 
+        {
+            int[]c = new int[a.Length];
+            for(int i = a.Length - b; i < a.Length; ++i)
+            {
+                c[i] = a[i - b];
+            }
+            for(int i = 0; i < b; ++i)
+            {
+                c[i] = a[i + b];
+            }
+            return c;
+        }
+        public string StringToHex(string input)
+        {
+            string hexOutput = "";
+            foreach (char c in input)
+            {
+                // Lấy mã ASCII của ký tự và chuyển nó thành chuỗi hex
+                hexOutput += ((int)c).ToString("X2") + " ";
+            }
+            return hexOutput.Trim(); // Trả về kết quả và loại bỏ khoảng trắng cuối
+        }
+
+        public void sinhKhoa(String s) 
+        {
+            k[0] = s;
+            for(int i = 0; i < numE / 4; i++)
+            {
+                w[i] = s.Substring(i * 4, 4);
+                txt_giai_thich.Text += $"w[{i}] = {StringToHex(w[i ])}\r\n";
+            }
+            //txt_giai_thich.Text += "K[0] : \r\n";
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //        txt_giai_thich.Text += CharToHex((Byte)(w[j][i])) + " ";
+            //        txt_giai_thich.Text += "\r\n";
+
+            //}
+            if (numE == 16)
+            {
+                for (int i = 4; i < 44; i++)
+                {
+                    
+
+                    // Tính w[i * 4]
+                    if(i % 4 == 0)
+                    {
+                        w[i ] = xor(g(w[i -1], i / 4), w[(i - 4)]);
+                        
+                       
+                    }
+                    else
+                    {
+                        w[i] = xor(w[i - 1], w[i - 4]);
+                        ;
+                    }
+                    if (string.IsNullOrEmpty(k[i / 4]))
+                        k[i / 4] = w[i];
+                    else
+                        k[i/4] += w[i];
+                    txt_giai_thich.Text += $"w[{i}] = {StringToHex(w[i])}\r\n";
+
+                    // Khởi tạo hoặc nối thêm vào k[i]
+
+
+                    // Tính w[i * 4 + j] cho j từ 1 đến 3
+
+
+
+
+                }
+
+            }
+
+
+
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            txt_giai_thich.Text = string.Empty;
+            txt_ma.Text = string.Empty;
+            if(txt_ro.Text.Length == 0)
+            {
+                MessageBox.Show("Chưa nhập bản rõ");
+                return;
+            }
+            if(rad128.Checked == false && rad192.Checked == false && rad256.Checked == false)
+            {
+                MessageBox.Show("Chưa chọn loại khóa");
+                return;
+            }
+            if (rad128.Checked == true)
+                numE = 16;
+            else if(rad192.Checked == true)
+                numE = 24;
+            else
+                numE = 32;
+            if(numE != txt_khoa.Text.Length)
+            {
+                MessageBox.Show("Độ dài khóa và loại khóa không bằng nhau");
+                return;
+            }
+            if(txt_ro.Text.Length % numE != 0)
+            {
+                txt_giai_thich.Text += $"Thêm {(numE -(txt_ro.Text.Length % numE))} ký tự X\r\n{txt_ro.Text} => ";
+            }
+            while (txt_ro.Text.Length % numE != 0) 
+            {
+                txt_ro.Text += "X";
+            }
+            txt_giai_thich.Text += txt_ro.Text + "\r\n";
+            sinhKhoa("+~\u0015\u0016(\u00AE\u00D2\u00A6\u00AB\u00F7\u0015\u0088\t\u00CFO<");
+
+        }
+    }
+}
